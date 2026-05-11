42538dcbabcdb8bc92b94827ce63f3fc4543fc96
Hager Tarek <hagert952@gmail.com>
2026-05-11 21:06:55 +0300

    fix firebase service account

--- dist/src/auth/entities/firebase-admin.js
+++ dist/src/auth/entities/firebase-admin.js
@@ -13,12 +13,14 @@ const firebaseAdminProvider = {
     provide: 'FIREBASE_ADMIN',
     useFactory: () => {
         if (admin.apps.length === 0) {
+            const serviceAccount = {
+                project_id: process.env.FIREBASE_PROJECT_ID,
+                client_email: process.env.FIREBASE_CLIENT_EMAIL,
+                private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+            };
+            console.log(serviceAccount);
             admin.initializeApp({
-                credential: admin.credential.cert({
-                    projectId: process.env.FIREBASE_PROJECT_ID,
-                    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-                    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-                }),
+                credential: admin.credential.cert(serviceAccount),
             });
         }
         return admin;
--- src/auth/entities/firebase-admin.ts
+++ src/auth/entities/firebase-admin.ts
@@ -1,25 +1,25 @@
- // import * as admin from 'firebase-admin';
-// admin.initializeApp({
-//   credential: admin.credential.applicationDefault(),
-// });
-
-// export { admin };
 import { Module, Global } from '@nestjs/common';
 import * as admin from 'firebase-admin';
 
 const firebaseAdminProvider = {
   provide: 'FIREBASE_ADMIN',
+
   useFactory: () => {
     if (admin.apps.length === 0) {
+
+      const serviceAccount = {
+        project_id: process.env.FIREBASE_PROJECT_ID,
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      };
+
+      console.log(serviceAccount);
+
       admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  }),
-});
- 
+        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+      });
     }
+
     return admin;
   },
 };
