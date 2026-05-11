1ecfd9270aa59c8fac2462b78b5274fef1284a98
Hager Tarek <hagert952@gmail.com>
2026-05-11 19:09:30 +0300

    resolve merge conflict

--- dist/src/app.module.js
+++ dist/src/app.module.js
@@ -86,6 +86,7 @@ exports.AppModule = AppModule = __decorate([
             typeorm_1.TypeOrmModule.forRoot({
                 type: "mysql",
                 host: process.env.DB_HOST,
+                port: process.env.DB_PORT,
                 username: process.env.DB_USERNAME,
                 password: process.env.DB_PASSWORD,
                 database: process.env.DB_DATABASE,
