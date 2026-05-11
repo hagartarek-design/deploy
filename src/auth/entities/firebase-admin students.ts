8eb02d8ebb6e67d2ff69f6932a1517d1a0f05338
Hager Tarek <hagert952@gmail.com>
2026-05-11 22:17:46 +0300

    first deploy

--- src/auth/entities/firebase-admin students.ts
+++ src/auth/entities/firebase-admin students.ts
@@ -1,32 +1,31 @@
-import { Module, Global } from '@nestjs/common';
-import * as admin from 'firebase-admin';
+// // import * as admin from 'firebase-admin';
+// // admin.initializeApp({
+// //   credential: admin.credential.applicationDefault(),
+// // });
 
-const firebaseAdminProvider = {
-  provide: 'FIREBASE_ADMIN',
+// // export { admin };
+// import { Module, Global } from '@nestjs/common';
+// import * as admin from 'firebase-admin';
 
-  useFactory: () => {
-    if (admin.apps.length === 0) {
+// const firebaseAdminProvider = {
+//   provide: 'FIREBASE_ADMIN',
+//   useFactory: () => {
+//     if (admin.apps.length === 0) {
+//       admin.initializeApp({
+//   credential: admin.credential.cert({
+//     projectId: process.env.FIREBASE_PROJECT_ID,
+//     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+//   }),
+// });
+//     }
+//     return admin;
+//   },
+// };
 
-      const serviceAccount = {
-        project_id: process.env.FIREBASE_PROJECT_ID,
-        client_email: process.env.FIREBASE_CLIENT_EMAIL,
-        private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      };
-
-      console.log(serviceAccount);
-
-      admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-      });
-    }
-
-    return admin;
-  },
-};
-
-@Global()
-@Module({
-  providers: [firebaseAdminProvider],
-  exports: [firebaseAdminProvider],
-})
-export class FirebaseAdminModule {}
+// @Global()
+// @Module({
+//   providers: [firebaseAdminProvider],
+//   exports: [firebaseAdminProvider],
+// })
+// export class FirebaseAdminModule {}
