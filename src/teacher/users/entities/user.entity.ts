444417f4713ea46c0ce5e8d77bceebc9f8a89236
Hager Tarek <hagert952@gmail.com>
2026-05-13 12:55:50 +0300

    first deploy

--- src/teacher/users/entities/user.entity.ts
+++ src/teacher/users/entities/user.entity.ts
@@ -1,59 +1,138 @@
-// import { userRoles } from "common/enums";
-// import { Student } from "src/students/entities/student.entity";
-// import { User } from "src/teacher/users/entities/user.entity";
-// import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Optional } from "@nestjs/common";
+import { IsOptional } from "class-validator";
+import { Assignment } from "../../../assignments/entities/assignment.entity";
+import { Exam } from "../../../teacher/exams/entities/exam.entity";
 
-// @Entity()
-// export class Role {
+import { Image } from "../../../teacher/images/entities/image.entity";
+import { Section } from "../../../sections/entities/section.entity";
+import { Column,JoinTable, CreateDateColumn, DeleteDateColumn, Entity, ManyToMany, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn, ManyToOne, JoinColumn, OneToOne } from "typeorm";
+import { Course } from "../../../courses/entities/course.entity";
+// import { Role } from "../../../role/entities/role.entity";
+import { Users } from "../../../users/entities/user.entity";
+@Entity()
 
-// @PrimaryGeneratedColumn()
-// id:number
-// @Column({nullable:true})
-// endpoint:string
-// @Column({nullable:true})
-// modules:string
-// @Column({unique:true,default:userRoles.USER})
-// name:string
-// @Column({default:false})
-// iscanstudent:boolean
-// @Column({default :false})
-// iscanteacher:boolean
-// @Column({nullable:true})
-// method:string
-// @OneToMany(()=>User,user=>user.role)
-// users:User[]
-// @OneToMany(()=>Student,student=>student.role)
-// student:Student[]
-// }
-// src/roles/entities/role.entity.ts
-import { Entity, Column, PrimaryGeneratedColumn,OneToMany } from 'typeorm';
-import { User } from "../../teacher/users/entities/user.entity";
-@Entity('roles') 
-export class Role {
-  @PrimaryGeneratedColumn()
+export class User {
+  @PrimaryGeneratedColumn() 
   id: number;
 
-  @Column()
-  module_name: string;
+  
 
-  @Column()
-  endpoint: string;
+  @Column({nullable :true,default:"1"})
+  grade:string
+  @Column({ unique: true, nullable: true })
+  @Optional()
+  email: string;
+  // @Column({nullable :true})
+  // role:string
+  provider:string;
+  @Column({ nullable: true })
+  password: string;
+  @Column({nullable: true})
+  @IsOptional()
+  phone?:string
+  @Column({nullable: true})
+  // @Column({nullable: true})
+  cardimg:string
+  @Column({nullable: true})
+  examcardimg:string
+  @Column({nullable: true})
+  assigcardimg:string
+  @Column({nullable:true})
+  username:string
+  @Column({nullable:true})
+  image:string
+  
+  @Column({ unique: true, nullable: true })
+  googleId: string;
+  
+  @Column({ nullable: true })
+  refreshToken: string; 
+  @Column({ nullable: true })
+  resetToken: string;
+
+  
+  
+
+  
+  
+  
+  @Column({ default: true ,nullable:true})
+  isActive: boolean;
+  @DeleteDateColumn()
+  deletedAt?: Date; 
+  @Column({nullable: true})
+  fullname:string
+  @OneToMany(() => Image, (image) => image.user, { cascade: true })
+  images: Image[];
+  @OneToMany(() => Section, (section) => section.userId, { cascade: true })
+  section: Section[];
+  @OneToMany(() => Assignment, (assignment) => assignment.userId, { cascade: true })
+  assignment: Assignment[];
+  @OneToMany(() => Exam, (exam) => exam.userId, { cascade: true })
+  exam: Exam[];
+  
+  
+    @ManyToMany(
+          () => Course, 
+          course => course.users,
+          {onDelete: 'NO ACTION', onUpdate: 'NO ACTION'})
+          @JoinTable({
+            name: 'user_course',
+            joinColumn: {
+              name: 'user_id',
+              referencedColumnName: 'id',
+            },
+            inverseJoinColumn: {
+              name: 'course_id',
+              referencedColumnName: 'id',
+            },
+          })
+          courses?: Course[];
+  
+
+  
+
+  
+
+
+
+// F
+
+
+// @Column({ unique: true })
+//   googleId: string;
+
+//   @Column()
+//   email: string;
+
   @Column({nullable:true})
   name: string;
 
-  @Column()
-  method: string;
+  @Column({ nullable: true })
+  picture: string;
 
-  @Column({ default: false })
-  can_teacher: boolean;
-@Column({nullable:true})
-query:string;
-  @Column({ default: false })
-  can_student: boolean;
-@OneToMany(()=>User,user=>user.role)
-users:User[]
-@OneToMany(()=>User,user=>user.roles) 
-user:User[]
-  // @Column({ default: false })
-  // can_user: boolean; 
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+// @ManyToOne(()=>Role,role=>role.users,)
+// @JoinColumn({name:'role_id'})
+// role:Role
+@Column({
+  type: 'varchar',
+  default: 'user',
+})
+roles: 'user' | 'student';
+//  @ManyToOne(() => Role, role => role.user, { eager: true })
+//   @JoinColumn({ name: 'role_id' })
+//   roles: Role;
+// user.entity.ts
+@Column({ default: 'user' })
+role: 'user' | 'student';
+// @Column({})
+@OneToOne(() => Users, users => users.teacher)
+user: Users;
+
 }
