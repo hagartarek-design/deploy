332e4c4258b989a7d94f19405cc86e05e071be68
Hager Tarek <hagert952@gmail.com>
2026-05-17 16:17:58 +0300

    first deploy

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ async function bootstrap() {
   app.enableCors({
     origin: true,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 //  app.enableCors({
